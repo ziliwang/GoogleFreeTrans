f59441d8009fbb4743927d2062f171122858bfcc
Zili Wang <wzlnot@gmail.com>
2019-03-28 09:48:51 +0800

    tkk updata, add retry to solve timeout exception.

--- GoogleFreeTrans/gettkk.py
+++ GoogleFreeTrans/gettkk.py
@@ -1,6 +1,11 @@
-import requests, re, execjs
+import requests
+import re
+import execjs
+import time
+
 
 def get_res(url):
+    """get raw html from 'https://translate.google.cn/' """
     try:
         res = requests.get(url, timeout = 1.5)
         res.raise_for_status()
@@ -10,19 +15,19 @@ def get_res(url):
         print('[-]ERROR: ' + str(ex))
         return res
 
-def find_tkk_fn(res):
-    re_tkk = r"TKK=eval\('(\(\(function\(\)\{.+?\}\)\(\)\))'\);"
-    tkk_fn = re.search(re_tkk, res)
-    return tkk_fn
 
 def get_tkk():
+    """get the tkk"""
     url = 'https://translate.google.cn/'
-    try:
-        res = get_res(url)
-        tkk_fn = find_tkk_fn(res.text)
-        content = tkk_fn.group(1).encode('utf-8').decode('unicode_escape')
-        tkk = execjs.eval(content)
-        return tkk
-    except Exception as ex:
-        print(ex)
-    
+    retry = 3
+    time_interval = 3
+    while retry > 0:
+        try:
+            res = get_res(url)
+            tkk = re.search(r'tkk\:\'(\d+\.\d+)?\'', res.text).group(1)
+            return tkk
+        except requests.exceptions.ReadTimeout as ex:
+            time.sleep(time_interval)
+            time_interval += 1
+            retry -= 1
+    raise requests.exceptions.ReadTimeout("can't visit https://translate.google.cn/")
